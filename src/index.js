ada200d00469d590bd23265f7dbed4e2846cc962
Hao Lam <tuonghao2001@gmail.com>
2023-08-16 02:08:07 -0700

    built profile card

--- src/index.js
+++ src/index.js
@@ -2,23 +2,70 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-function Avatar() {}
+function Avatar(props) {
+  return <img className="avatar" src={props.path} alt={props.name} />;
+}
 
-function Intro() {}
+function Intro(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.introduction}</p>
+    </div>
+  );
+}
 
-function SkillList() {}
+function SkillList(props) {
+  const skills = props.skills.map((info, idx) => (
+    <Skill
+      key={idx}
+      skill={info.skill}
+      backgroundColor={info.color}
+      emoji={info.emoji}
+    />
+  ));
+  return <ul className="skill-list">{skills}</ul>;
+}
+
+function Skill(props) {
+  return (
+    <li className="skill" style={{ backgroundColor: props.backgroundColor }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </li>
+  );
+}
 
 function App() {
   return (
     <div className="card">
-      <Avatar />
+      <Avatar path="Profile.png" name="Profile Picture" />
       <div className="data">
-        <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
-        <SkillList />
-        {/* <img src="Profile.png" /> */}
+        <Intro
+          name="Hao Tuong Lam"
+          introduction="Full-stack web developer and Embedded engineer. When I am free, I like to working out, reading, playing billiards and hanging out with my brothers."
+        />
+        <SkillList
+          skills={[
+            { skill: "HTML", color: "red", emoji: "💪" },
+            { skill: "CSS", color: "yellow", emoji: "🫵" },
+            { skill: "JS", color: "burlywood", emoji: "🫰" },
+            { skill: "Node", color: "pink", emoji: "👌" },
+            { skill: "Express", color: "coral", emoji: "🤛" },
+            { skill: "React", color: "orange", emoji: "✌️" },
+            { skill: "Ruby", color: "lightblue", emoji: "🫴" },
+            { skill: "Computer Architechture", color: "cyan", emoji: "👋" },
+            {
+              skill: "Assembly (x86-64, RISC-V)",
+              color: "chocolate",
+              emoji: "🙌",
+            },
+            { skill: "C/C++", color: "darkkhaki", emoji: "🙏" },
+            { skill: "IoT", color: "cornflowerblue", emoji: "👐" },
+            { skill: "FPGA", color: "violet", emoji: "🫶" },
+            { skill: "Trilingual", color: "aquamarine", emoji: "🤲" },
+          ]}
+        />
       </div>
     </div>
   );
